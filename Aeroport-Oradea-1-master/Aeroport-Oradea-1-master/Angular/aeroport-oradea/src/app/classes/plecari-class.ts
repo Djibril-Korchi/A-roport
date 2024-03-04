1ccf5ac60ec646ca36e1b6925a4c0e4da5fe1c69
CHIHI_Ya <yacinechihi8@gmail.com>
2024-03-04 16:18:38 +0100

    feat:template

---
+++ Aeroport-Oradea-1-master/Aeroport-Oradea-1-master/Angular/aeroport-oradea/src/app/classes/plecari-class.ts
@@ -0,0 +1,9 @@
+export class PlecariClass {
+    deserialize(res: any) {
+        throw new Error("Method not implemented.");
+    }
+    destination: any;
+    flight_number: string;
+    date: string;
+    airline: string;
+}
---
+++ Aeroport-Oradea-1-master/Aeroport-Oradea-1-master/Angular/aeroport-oradea/src/app/header-tabele/header-tabele.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header-tabele',
+  templateUrl: './header-tabele.component.html',
+  styleUrls: ['./header-tabele.component.css']
+})
+export class HeaderTabeleComponent implements OnInit {
+  currentDate = new Date();
+
+  @Input() name: string;
+
+  constructor() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1);
+   }
+  ngOnInit(): void {
+  }
+
+}
